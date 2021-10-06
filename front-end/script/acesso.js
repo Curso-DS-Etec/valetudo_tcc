f2b5986fc7fc168d709da77b0472edcaab94e19f
semiDeus8472 <lucas.souza8472@gmail.com>
2021-10-06 17:27:03 -0300

    criado a pasta script com dois arquivos dentro
    criado a pasta script com o acesso.js e o jquery.js

---
+++ front-end/script/acesso.js
@@ -0,0 +1,41 @@
+$(function(){
+    $("button#btnEntrar").on("click", function(e){
+        e.preventDefault();
+
+        var campoUsuario = $("form#formularioLogin #usuarioProfissional").val();
+        var campoSenha = $("form#formularioLogin #senhaProfissional").val();
+
+        if(campoUsuario.trim() == "" || campoSenha.trim() == ""){
+            $("div#mensagem").html("Preencha todos os campos.");
+        }else{
+            $.ajax({
+                url: "../back-end/login.php",
+                type: "POST",
+                data: {
+                    // nome do post "email" e valor do post "campoEmail"
+                    usuarioProfissional: campoUsuario,
+                    senhaProfissional: campoSenha
+                },
+
+                success: function(retorno){
+                    retorno = JSON.parse(retorno);
+
+                    if(retorno["erro"]){
+                        $("div#mensagem").html(retorno["mensagem"]);
+                    }else{
+                        window.location = "dashboard.php";
+                    }
+                },
+
+                error: function(){
+                    $("div#mensagem").html("Ocorreu um erro durante a solicitação");
+                }
+            });
+        }
+    });
+});
+
+// verifica se o jquery esta funcionando
+// $(function(){
+//     alert("ok");
+// });

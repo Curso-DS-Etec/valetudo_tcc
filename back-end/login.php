ac04e1648a672db20ab30fffea51e12777fb0c16
semiDeus8472 <lucas.souza8472@gmail.com>
2021-10-28 21:31:57 -0300

    atualizacao do login.php
    atualização do login.php deixando de usar o php orientado a obejeto

--- back-end/login.php
+++ back-end/login.php
@@ -1,22 +1,17 @@
+
 <?php
-    require("conexao2.php");
+include("conexao.php");
 
-    if(isset($_POST["usuarioProfissional"]) && isset($_POST["senhaProfissional"]) && $con != null){
+$varUsuario = $_POST["usuarioProfissional"]; 
+$varSenha = $_POST["senhaProfissional"];
 
-        $query = $con->prepare("SELECT * FROM dadosLogin WHERE username = ? AND senha = ?");
-        $query->execute(array($_POST["usuarioProfissional"], $_POST["senhaProfissional"]));
 
-        if($query->rowCount()){
-            $user = $query->fetchAll(PDO::FETCH_ASSOC)[0];
+$query = mysqli_query($conexao, "SELECT * FROM dadosLogin WHERE username = '$varUsuario' AND senha = '$varSenha'");
 
-            session_start();
-            $_SESSION["username"] = array($user["nome"], $user["adm"]);
-                    
-                    echo json_encode(array("erro" => 0));
-                }else{
-                    echo json_encode(array("erro" => 1, "mensagem" => "Email e/ou senha incorretos."));
-                }
-            }else{
-                echo json_encode(array("erro" => 1, "mensagem" => "Ocorreu um erro interno no servidor."));
-            }
-        ?>
+if(mysqli_num_rows($query) == 1){
+	header("location:../front-end/dashboard.php?id=".$varUsuario);	
+} else {
+	echo "Usuário ou senha inválidos";
+}
+
+?>

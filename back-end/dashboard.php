1eee49ce29897d6a3ac98802c4c75486ee8c7155
semiDeus8472 <lucas.souza8472@gmail.com>
2021-10-28 21:29:01 -0300

    atualizacao da dashboard sem o jquery
    atualizacao da dashboard sem o jquery

--- back-end/dashboard.php
+++ back-end/dashboard.php
@@ -1,56 +1,19 @@
 <?php
- require("../back-end/conexao2.php");
-    session_start();
+include("../back-end/conexao.php");
 
-    if(isset($_SESSION["username"]) && is_array($_SESSION["username"])){
-        require("../back-end/conexao2.php");
+$varUsuario = $_GET["id"];
 
-        $adm  = $_SESSION["username"][1];
-        $nome = $_SESSION["username"][0];
-    }else{
-        echo "<script>window.location = 'cadastroLoginProfissional'</script>";
-    }
+$query = mysqli_query($conexao,"SELECT * FROM dadosLogin WHERE username = '$varUsuario'" );
+while($exibe = mysqli_fetch_array($query)){
+	
 ?>
-<html>
-    <head>
-        <title>Dashboard Usuário - <?php echo $nome; ?></title>
-    </head>
-    <body>
-        <?php if($adm): ?>
-            <table width="40%">
-                <thead>
-                    <tr style="font-weight: bold">
-                        <td>Email</td>
-                        <td>Senha</td>
-                        <td>Nome</td>
-                        <td>ADM</td>
-                        <td>ID</td>
-                        <td>Username</td>
-                    </tr>                
-                </thead>
-                <tbody>
-                    <?php
-                        $query = $con->prepare("SELECT * FROM dadosLogin");
-                        $query->execute();
-                
-                        $users = $query->fetchAll(PDO::FETCH_ASSOC);
 
-                        for($i = 0; $i < sizeof($users); $i++):
-                            $usuarioAtual = $users[$i];
-                    ?>
-                    <tr>
-                        <td><?php echo $usuarioAtual["email"]; ?></td>
-                        <td><?php echo $usuarioAtual["senha"]; ?></td>
-                        <td><?php echo $usuarioAtual["nome"]; ?></td>
-                        <td><?php echo $usuarioAtual["adm"]; ?></td>
-                        <td><?php echo $usuarioAtual["idUsuario"]; ?></td>
-                        <td><?php echo $usuarioAtual["username"]; ?></td>
-                    </tr>
-                    <?php endfor; ?>
-                </tbody>            
-            </table>
-        <?php endif; ?>
+<?php echo $exibe[1] ?><br>
+<?php echo $exibe[2] ?><br>
+<?php echo $exibe[3] ?><br>
+<?php echo $exibe[7] ?><br>
 
-        <a href="../back-end/logout.php">Sair</a>
-    </body>
-</html>
+<?php } ?>
+
+
+<a href="../back-end/logout.php">Sair</a>

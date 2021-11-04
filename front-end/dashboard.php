31e4b72bbac00d2e586d3c4761bfb8275849ca51
dioguera <di.silvargt@gmail.com>
2021-11-04 19:47:30 -0300

    mudança de extensão

--- front-end/dashboard.php
+++ front-end/dashboard.php
@@ -105,29 +105,8 @@
                         </div>
                     </div>
 
-                    <?php
-                                    include("../back-end/conexao.php");
-
-                                    $varUsuario = $_POST["usuarioProfissional"]; 
-                                    $varSenha = $_POST["senhaProfissional"];
-
-
-                                    $query = mysqli_query($conexao, "SELECT * FROM dadosLogin WHERE username = '$varUsuario' AND senha = '$varSenha'");
-                                    
-                                    if(mysqli_num_rows($query) == 1){
-                                        header("location:../front-end/dashboard.php?id=".$varUsuario);	
-                                    } else {
-                                        echo "Usuário ou senha inválidos";
-                                    }
-                                    
-                                    
-                   
-                   ?>
-
-
-
                     <div class="sb-sidenav-footer">
-                        <div class="small">Logado como: <?php echo $varUsuario; ?> </div>
+                        <div class="small">Logado como: <?php echo ".$_REQUEST['id']."; ?> </div>
                     </div>
                 </nav>
             </div>
